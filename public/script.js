1d369dc76cd0ad210169ec5fa91fc93dca571a5b
BenedictSerra <b2029bjvserra@pshs.edu.ph>
2025-08-12 08:50:26 +0800

    Speed Test 2

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@
 
 
 
-let age = window.prompt("Enter your age")
+/*let age = window.prompt("Enter your age")
 let maxAge = window.prompt("Enter your max age")
 let numPerDay = window.prompt("Enter the amount of buldak you consume per day")
 if(confirm(`Are the values you entered for your age, max age and buldak really true?`)) {
@@ -19,3 +19,5 @@ totalRequired = (numPerDay * 365) * (maxAge - age);
 
 //document.write("You will need " + totalRequired + " packs of buldak to last you until the ripe old age of " + maxAge); 
 alert(`You will need ${totalRequired} Buldak to last you to the ripe old age of ${maxAge}`);
+
+*/
